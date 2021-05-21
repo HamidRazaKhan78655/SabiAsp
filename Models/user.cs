70dc5f680fa40e79bf40ab9ce091bb50a2df7917
NewMoon <NewMoon@DESKTOP-9OFVSQD>
2021-05-21 18:07:51 +0500

    add col

--- Models/user.cs
+++ Models/user.cs
@@ -38,6 +38,7 @@ namespace SabiAsp.Models
         public Nullable<System.DateTime> CreatedDate { get; set; }
         public Nullable<int> ModifiedBy { get; set; }
         public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public string SocialLoginType { get; set; }
     
         public virtual Role Role { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
