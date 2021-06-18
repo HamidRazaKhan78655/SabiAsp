0f0f97c723e7d067c02f159b362f8cc006c29e87
Hamid Raza <hamidraza78655@gmail.com>
2021-06-18 13:05:53 +0500

    Cart Design

---
+++ Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SabiAsp.Models
+{
+    public class CartItem
+    {
+        public Cart cart { get; set; }
+        public item item { get; set; }
+    }
+}
